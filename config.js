29957b644d7e3d6a41b7a36c9458b945668dd828
Hai2ker <95692333+Hai2ker@users.noreply.github.com>
2021-12-07 19:58:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Vợ iu <3",
+    introTitle: 'Đỗ Thị Hồng Sen',
+    introDesc: `Anh chỉ mún nói là!
+    Yêu em anh để nằm ngang. 
+    Tại vì anh muốn yêu em ngàn năm <3!`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy (hong được tắt đi nha tắt là anh giận á)',
+    btnYes: 'Yêu anh nhất thế giới lun <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Anh có món quà này cho em nè em truy cập vào đường link này nha: https://underfed-commanders.000webhostapp.com/.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
